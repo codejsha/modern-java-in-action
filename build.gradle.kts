6de704499f06d3b3e3e1aa1f8a14c00ed3074f78
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-10-10 12:55:11 +0900

    migrate from maven to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("src/main/java")
+        }
+        resources {
+            srcDirs("src/main/resources")
+        }
+    }
+    test {
+        java {
+            srcDirs("src/test/java")
+        }
+        resources {
+            srcDirs("src/test/resources")
+        }
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.24")
+    implementation("org.slf4j:slf4j-api:2.0.3")
+    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.javatuples:javatuples:1.2")
+    implementation("org.openjdk.jmh:jmh-core:1.35")
+    implementation("org.openjdk.jmh:jmh-generator-annprocess:1.35")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.hamcrest:hamcrest-library:2.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+    }
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+}
