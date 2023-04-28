785854bb7e6b8a5504c4762bd0b6677c2ffb8cc4
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-04-29 06:40:32 +0900

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,23 +35,24 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.24")
-    implementation("org.slf4j:slf4j-api:2.0.3")
-    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("org.javatuples:javatuples:1.2")
+    implementation("org.projectlombok:lombok:1.18.26")
+    implementation("org.slf4j:slf4j-api:2.0.5")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
     implementation("org.openjdk.jmh:jmh-core:1.35")
     implementation("org.openjdk.jmh:jmh-generator-annprocess:1.35")
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.hamcrest:hamcrest-library:2.2")
-    testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.2.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.26")
 }
 
 tasks {
